c1fbf8dbf1e6753a7882df4547280d52ec2749ee
zhousiyuan114 <zhousiyuan0114@gmail.com>
2021-03-08 11:36:36 +0100

    First Pom Configuration

---
+++ src/test/java/tp10/metier/ActionCompliqueeTest.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp10.metier;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author lenovo
+ */
+public class ActionCompliqueeTest {
+    
+    public ActionCompliqueeTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+    
+}
